4f3d0194b7999e16d0e945bfc7595a7397169a53
pierrenad <47671651+pierrenad@users.noreply.github.com>
2019-04-12 17:00:18 +0200

    12-04-19 (2)

--- FlightAndAirport/User.cs
+++ FlightAndAirport/User.cs
@@ -8,5 +8,35 @@ namespace FlightAndAirport
 {
     class User
     {
+        #region 
+        private string _nom;
+        private string _mdp;
+        #endregion 
+
+        #region PROPRIETES 
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Mdp
+        {
+            get { return _mdp; } 
+            set { _mdp = value; }
+        }
+        #endregion 
+
+        public User()
+        {
+            Nom = "";
+            Mdp = ""; 
+        }
+
+        public User(string n, string m)
+        {
+            Nom = n;
+            Mdp = m; 
+        }
     }
 }
